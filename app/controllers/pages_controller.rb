2c6004888c609e8f3893fd1d42c3fe52735c7e21
Verbeck <jverbe3@gmail.com>
2018-03-18 14:24:30 -0400

    Fixing home button with our wedding page...still needs work

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
   def index
+    respond_to do |format|
+      format.html {render :template => 'pages/index' }
+      format.js
+    end    
   end
 
   def jack_rose
