560aa5f14f4ca8283e1011810d357d3c2c8ae225
Verbeck <jverbe3@gmail.com>
2018-05-28 18:22:48 -0400

    Removing everything!

--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  root 'pages#index'
-
-  get 'pages/jack_rose', to: "pages#jack_rose", as: "jack_rose"
-  get 'pages/our_wedding', to: "pages#our_wedding", as: "our_wedding"
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
