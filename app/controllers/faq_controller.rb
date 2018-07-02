60665305165c11b80061d1d620fa582078f2837c
Verbeck <jverbe3@gmail.com>
2018-07-01 23:40:31 -0400

    Adding faq views

--- app/controllers/faq_controller.rb
+++ app/controllers/faq_controller.rb
@@ -5,4 +5,56 @@ class FaqController < ApplicationController
         erb :'faq/faq'
     end
 
+    get '/rsvp' do
+        erb :'faq/faq_rsvp'
+    end
+
+    get '/stay' do
+        erb :'faq/faq_stay'
+    end
+
+    get '/location' do
+        erb :'faq/faq_location'
+    end
+
+    get '/arrive_time' do
+        erb :'faq/arrive_time'
+    end
+
+    get '/getting_there' do
+        erb :'faq/getting_there'
+    end
+
+    get '/dinner' do
+        erb :'faq/dinner_drinks'
+    end
+
+    get '/car' do
+        erb :'faq/car'
+    end
+
+    get '/attire' do
+        erb :'faq/attire'
+    end
+
+    get '/children' do
+        erb :'faq/children'
+    end
+
+    get '/guest' do
+        erb :'faq/guest'
+    end
+
+    get '/other_events' do
+        erb :'faq/other_events'
+    end
+
+    get '/pets' do
+        erb :'faq/pets'
+    end
+    
+    get '/taxi' do
+        erb :'faq/taxi'
+    end
+
 end
